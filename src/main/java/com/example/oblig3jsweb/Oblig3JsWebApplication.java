69bc2878f16e25ddfd593873fa499fa515ff9e0e
Gabebabe009 <gabal8187@oslomet.no>
2024-04-26 14:34:49 +0200

    -Initial files

---
+++ src/main/java/com/example/oblig3jsweb/Oblig3JsWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig3jsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3JsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3JsWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig3jsweb/Oblig3JsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig3jsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3JsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
